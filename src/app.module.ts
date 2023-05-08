862b4d8fb5d3566addb63fae52a4fc32df9edde0
Camilo Vargas <camilovargasingm@gmail.com>
2023-05-08 03:27:29 -0500

    test deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MutantModule } from './mutant/mutant.module';
 import { StatsModule } from './stats/stats.module';
 
+const db_options = {
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
 @Module({
   imports: [
     TypeOrmModule.forRoot({
