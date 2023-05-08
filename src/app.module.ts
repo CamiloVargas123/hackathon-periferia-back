138a5ff19eeb73642ffefd99f316fd072a8d78db
Camilo Vargas <camilovargasingm@gmail.com>
2023-05-08 06:16:55 -0500

    conection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MutantModule } from './mutant/mutant.module';
 import { StatsModule } from './stats/stats.module';
-
-const db_options = {
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-};
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'sql9.freesqldatabase.com',
       port: 3306,
-      username: 'root',
-      password: '',
-      database: 'hackathon',
+      username: 'sql9616790',
+      password: '5P2yHkrkmc',
+      database: 'sql9616790',
       entities: [__dirname + '/../**/*.entity.js'],
       synchronize: true,
     }),
